37e5804fb055d128055cf9ab881fe5e1f20e721f
Jacob Danovitch <jacob.danovitch@mila.quebec>
2023-01-06 15:54:50 +0000

    got local devcontainer setup

--- .devcontainer/scripts/post_create.sh
+++ .devcontainer/scripts/post_create.sh
@@ -1,1 +1,13 @@
-mkdir -p ~/.config/sops/age && echo $SOPS_AGEKEY > ~/.config/sops/age/keys.txt
+FILE=${SOPS_KEY_PATH:-~/.config/sops/age/keys.txt}
+
+if [ -f "$FILE" ]; then
+    echo "$FILE exists."
+    exit 0;
+elif [ -z "${SOPS_AGEKEY}" ]; then
+    echo "SOPS_AGEKEY not set. Exiting."
+    exit 1;
+else
+    mkdir -p $(dirname $FILE)
+    echo $SOPS_AGEKEY > $FILE
+    "Copied SOPS_AGEKEY to $FILE."
+fi
